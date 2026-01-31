5edaea4d357523c19a7615b946301bbc1b2487b7
RaishVakaliya <raishvakaliya@gmail.com>
2026-02-01 02:01:36 +0530

    show actual stories at Index page

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,17 +1,32 @@
 import Story from "./Story";
-import { STORIES } from "@/constants/mock-data";
 import { styles } from "@/styles/feed.styles";
 import { ScrollView } from "react-native";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const StoriesSection = () => {
+  const stories = useQuery(api.stories.getStories);
+
+  if (stories === undefined || stories.length === 0) {
+    return null;
+  }
+
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
       style={styles.storiesContainer}
     >
-      {STORIES.map((story) => (
-        <Story key={story.id} story={story} />
+      {stories.map((story) => (
+        <Story
+          key={story._id}
+          story={{
+            id: story._id,
+            username: story.username,
+            avatar: story.avatar,
+            hasStory: story.hasStory,
+          }}
+        />
       ))}
     </ScrollView>
   );
